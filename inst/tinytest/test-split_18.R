d5be3c90cd4ac2b2be0fc8b3c2d03d6d20f2b502
Arne Johannes Holmin <arnejh@hi.no>
2022-12-01 14:34:24 +0100

    Fixed bug when running a project with projectPath ending with exactly one slash (problem fixed in getRelativePath()). Removed rows of the output from ReportBootstrap() that contained combinations of the GroupingVariables that are not present in the BootstrapData. There rows were created to ensure that all bootstrap runs contain all combinations of the GroupingVariables, but also introduced non-existing combinations. Added unfinished PlotReportBootstrap(). Prepared for adding tolerance to tests. Improved documentation of seed in Bootstrap(). Fixed bug in pkgdown.yaml. Added support for pre-releases, which are not deployed to the StoX repo. Updated tests to document the StratumArea change when switching to sf.

--- inst/tinytest/test-split_18.R
+++ inst/tinytest/test-split_18.R
@@ -1,3 +1,11 @@
 # Run the test project:
 projectPaths <- system.file("test",  "splitNASC_18.zip", package = "RstoxFramework")
 expect_true(compareProjectToStoredOutputFiles(projectPaths, ignore = c("MinLayerDepth", "MaxLayerDepth")))
+
+# Stratum area is identical for the simple shapes of this project (based on a grid):
+d <- compareProjectToStoredOutputFiles(projectPaths, ignore = c("MinLayerDepth", "MaxLayerDepth"), data.out = TRUE, tolerance = 1e-12)
+expect_true(all(lengths(d$test) == 0))
+
+# In RstoxFramework 3.6.0 the splitNASC_18 test project should be updated and the test reset to the following:
+#projectPaths <- system.file("test",  "splitNASC_18.zip", package = "RstoxFramework")
+#expect_true(compareProjectToStoredOutputFiles(projectPaths, tolerance = 1e-12))

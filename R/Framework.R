4249cab0a21175ac836c831e700d4d844d1ef7c2
Arne Johannes Holmin <arnejh@hi.no>
2023-05-01 21:37:57 +0200

    Test getDependentPackageVersion() on R <= 4.2.

--- R/Framework.R
+++ R/Framework.R
@@ -1258,13 +1258,16 @@ getDependentPackageVersion <- function(
     # Read the package table from the repos, using the first lib as the StoX GUI selects a folder in some cases on Windows and otherwise we can assume that the first should be used:
     packageTable <- as.data.frame(utils::installed.packages(.libPaths()[1]), stringsAsFactors = FALSE)
     
+    
     # Get the dependencies: 
     deps <- unique(unlist(tools::package_dependencies(packages = packageName, db = packageTable, recursive = recursive, which = if(is.na(dependencyTypes)) "strong" else dependencyTypes)))
+    warning("___111___ ", deps, " ___111___")
     
-    # Ignore base packages of R (but include recommended packates, which are also shipped with R but can be installed manually):
+    # Ignore base packages of R (but include recommended packages, which are also shipped with R but can be installed manually):
     deps <- setdiff(deps, rownames(utils::installed.packages(priority = "base")))
+    warning("___222___ ", deps, " ___222___")
     
-    # Igngore also Rstox packages:
+    # Ignore also Rstox packages:
     deps <- deps[!startsWith(deps, "Rstox")]
     
     
